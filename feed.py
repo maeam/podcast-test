72b365824c6b4b2418c274c7d8c137dbbed6e686
maeam <mae.am.bones@gmail.com>
2026-04-13 12:35:27 +0100

    Add link attribute

--- feed.py
+++ feed.py
@@ -10,7 +10,17 @@ with open('feed.yaml', 'r') as file:
         'xmlns:content':'http://purl.org/rss/1.0/modules/content/'})
 
 channel_element = xml_tree.SubElement(rss_element, 'channel')
+
+
+link_prefix = yaml_data['link']
+
 xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
+xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
+xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle']
+xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['author']
+xml_tree.SubElement(channel_element, 'description').text = yaml_data['title']
+xml_tree.SubElement(channel_element, 'itunes:image').text = yaml_data['image']
+xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
 
 output_tree = xml_tree.ElementTree(rss_element)
 output_tree.write('podcast.xml', encoding='UTF-8', xml_declaration=True)
